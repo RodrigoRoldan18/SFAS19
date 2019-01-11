376795f2fc23fdc6a561af06c1449077e4430f29
Rodrigo Roldan-Romero <luisrodrigoroldan@hotmail.com>
2019-01-11 15:59:31 +0000

    Coin is added to the game with its material, its particle effect and collision box

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour {
+
+    public Transform coinEffect;
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            //Add functionality for coin counter
+            var effect = Instantiate(coinEffect, transform.position, transform.rotation);
+            Destroy(effect.gameObject, 3);
+            Destroy(gameObject); 
+        }
+    }
+}
